507966c39aabc9eeefd445cac9fe58580ef3a3d7
Tung X. Dao <tungxdao.gmail.com>
2018-03-17 13:57:45 -0700

    sudoku completed algorithm

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -3,10 +3,44 @@ def check_validity(puzzle, i_check = [], j_check = []):
     
     if i and j are given, it only checks the validity of that square in the puzzle'''
 
+    if i_check != [] and j_check != []:
+        row = [False]*9
+        col = [False]*9
+        sqr = [False]*9
+        # check rows
+        for i in range(9):
+            if puzzle[i_check][i] - 1 < 0:
+                continue
+            elif row[puzzle[i_check][i]-1] == False:
+                row[puzzle[i_check][i]-1] = True
+            else:
+                return False
+        #check cols
+        for i in range(9):
+            if puzzle[i][j_check] - 1 < 0:
+                continue
+            elif col[puzzle[i][j_check]-1] == False:
+                col[puzzle[i][j_check]-1] = True
+            else:
+                return False
+        i_sq = i_check // 3
+        j_sq = j_check // 3
+        for i in range(0 + 3*i_sq, 3 + 3*i_sq):
+            for j in range(0 + 3*j_sq, 3 + 3*j_sq):
+                if puzzle[i][j] - 1 < 0:
+                    continue
+                elif sqr[puzzle[i][j] - 1] == False:
+                    sqr[puzzle[i][j] - 1] = True
+                else:
+                    return False
+        return True
+
+
     # check if rows, columns are valid
     for i in range(9):
         row = [False]*9
         col = [False]*9
+        
         # check rows
         for j in range(9):
             if puzzle[i][j]-1 < 0 :
@@ -15,7 +49,7 @@ def check_validity(puzzle, i_check = [], j_check = []):
                 row[puzzle[i][j]-1] = True
             else:
                 return False
-        #check cols
+        # check cols
         for j in range(9):
             if puzzle[j][i]-1 < 0 :
                 continue
@@ -23,9 +57,17 @@ def check_validity(puzzle, i_check = [], j_check = []):
                 col[puzzle[j][i]-1] = True
             else:
                 return False
-        #check square
-        for j in range(9):
-            pass
+        # check squares
+        for k in range(3):
+            sqr = [False]*9
+            for i in range(0 + 3*k, 3 + 3*k):
+                for j in range(0 + 3*k, 3 + 3*k):
+                    if puzzle[i][j] - 1 < 0:
+                        continue
+                    elif sqr[puzzle[i][j] - 1] == False:
+                        sqr[puzzle[i][j] - 1] = True
+                    else:
+                        return False
     return True
     
 def solver(puzzle):
@@ -41,14 +83,13 @@ def solver(puzzle):
                 continue
             else:
                 for guess in range(1, 10):
-                    new_puzzle = [x[:] for x in puzzle]
-                    new_puzzle[i][j] = guess
-                    if check_validity(new_puzzle):
-                        puzzle = solver(new_puzzle)
-                        break
-                    else:
-                        continue
-    return puzzle
+                    puzzle[i][j] = guess
+                    if check_validity(puzzle, i, j):
+                        if solver(puzzle):
+                            return True
+                    puzzle[i][j] = 0
+                return False
+    return True
 
 valid_puzzle = [[3, 9, 1, 2, 8, 6, 5, 7, 4],\
     [4, 8, 7, 3, 5, 9, 1, 2, 6],\
@@ -97,15 +138,29 @@ incomplete_puzzle = [[3, 0, 1, 2, 8, 6, 5, 7, 4],\
 # print(check_validity(invalid_puzzle_col))
 # print(check_validity(incomplete_puzzle))
 
-last_square_missing = [[0, 9, 1, 2, 8, 6, 5, 7, 4],\
-    [4, 8, 7, 3, 5, 9, 1, 2, 6],\
-    [6, 5, 2, 7, 1, 4, 8, 3, 9],\
-    [8, 7, 5, 4, 3, 1, 6, 9, 2],\
-    [2, 1, 3, 9, 6, 7, 4, 8, 5],\
-    [9, 6, 4, 5, 2, 8, 7, 1, 3],\
-    [1, 4, 9, 6, 7, 3, 2, 5, 8],\
-    [5, 3, 8, 1, 4, 2, 9, 6, 7],\
-    [7, 2, 6, 8, 9, 5, 3, 4, 1]]
+# last_square_missing = [[0, 9, 1, 2, 8, 6, 5, 7, 4],\
+#     [4, 8, 7, 3, 5, 9, 1, 2, 6],\
+#     [6, 5, 2, 7, 1, 4, 8, 3, 9],\
+#     [8, 7, 5, 4, 3, 1, 6, 9, 2],\
+#     [2, 1, 3, 9, 6, 7, 4, 8, 5],\
+#     [9, 6, 4, 5, 2, 8, 7, 1, 3],\
+#     [1, 4, 9, 6, 7, 3, 2, 5, 8],\
+#     [5, 3, 8, 1, 4, 2, 9, 6, 7],\
+#     [7, 2, 6, 8, 9, 5, 3, 4, 1]]
 
-print(solver(last_square_missing))
-print(solver(incomplete_puzzle))
+# solver(last_square_missing)
+# print(last_square_missing)
+# print(solver(incomplete_puzzle))
+
+standard_puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0], \
+    [6, 0, 0, 1, 9, 5, 0, 0, 0], \
+    [0, 9, 8, 0, 0, 0, 0, 6, 0], \
+    [8, 0, 0, 0, 6, 0, 0, 0, 3], \
+    [4, 0, 0, 8, 0, 3, 0, 0, 1], \
+    [7, 0, 0, 0, 2, 0, 0, 0, 6], \
+    [0, 6, 0, 0, 0, 0, 2, 8, 0], \
+    [0, 0, 0, 4, 1, 9, 0, 0, 5], \
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+
+solver(standard_puzzle)
+print(standard_puzzle)
