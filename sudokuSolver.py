3aac683c846c1259e1ed7ef43d72270e25f3e107
Tung X. Dao <tungxdao.gmail.com>
2018-03-17 12:24:17 -0700

    Sudoku implement recursion

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -40,11 +40,15 @@ def solver(puzzle):
             if puzzle[i][j] > 0:
                 continue
             else:
-                for number in range(9):
-                    # check if number provides a valid path
-                    break
-    solved_puzzle = []
-    return solved_puzzle
+                for guess in range(1, 10):
+                    new_puzzle = [x[:] for x in puzzle]
+                    new_puzzle[i][j] = guess
+                    if check_validity(new_puzzle):
+                        puzzle = solver(new_puzzle)
+                        break
+                    else:
+                        continue
+    return puzzle
 
 valid_puzzle = [[3, 9, 1, 2, 8, 6, 5, 7, 4],\
     [4, 8, 7, 3, 5, 9, 1, 2, 6],\
@@ -87,8 +91,21 @@ incomplete_puzzle = [[3, 0, 1, 2, 8, 6, 5, 7, 4],\
     [0, 2, 6, 8, 9, 5, 3, 4, 1]] 
 
 
-print(check_validity(valid_puzzle))
-print(check_validity(invalid_puzzle))
-print(check_validity(invalid_puzzle_col))
-print(check_validity(incomplete_puzzle))
 
+# print(check_validity(valid_puzzle))
+# print(check_validity(invalid_puzzle))
+# print(check_validity(invalid_puzzle_col))
+# print(check_validity(incomplete_puzzle))
+
+last_square_missing = [[0, 9, 1, 2, 8, 6, 5, 7, 4],\
+    [4, 8, 7, 3, 5, 9, 1, 2, 6],\
+    [6, 5, 2, 7, 1, 4, 8, 3, 9],\
+    [8, 7, 5, 4, 3, 1, 6, 9, 2],\
+    [2, 1, 3, 9, 6, 7, 4, 8, 5],\
+    [9, 6, 4, 5, 2, 8, 7, 1, 3],\
+    [1, 4, 9, 6, 7, 3, 2, 5, 8],\
+    [5, 3, 8, 1, 4, 2, 9, 6, 7],\
+    [7, 2, 6, 8, 9, 5, 3, 4, 1]]
+
+print(solver(last_square_missing))
+print(solver(incomplete_puzzle))
