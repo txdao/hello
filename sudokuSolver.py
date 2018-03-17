e17359a48f1fbe91126898c27582ab0e3d69c8b9
Tung X. Dao <tungxdao.gmail.com>
2018-03-16 20:26:15 -0700

    Sudoku solver initial commit

---
+++ sudokuSolver.py
@@ -0,0 +1,20 @@
+def check_validity(puzzle)
+    ''' Given a puzzle, returns if the puzzle is in a valid state.'''
+
+    return True
+    
+def solver(puzzle):
+    ''' Solves sudoku puzzle using brute force search.
+
+    <puzzle> is a 9x9 matrix.
+    Blank squares are indicated with zeros'''
+
+    # Check each square withouth a number
+    for i in range(9):
+        for j in range(9):
+            for number in range(9):
+                # check if number provides a valid path
+                
+
+    return solved_puzzle
+
